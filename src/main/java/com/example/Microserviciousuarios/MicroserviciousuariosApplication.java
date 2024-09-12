7acad4810ebc30093ecb10fb00d4709d4f916e52
veronica <105020892+vero2002257257@users.noreply.github.com>
2024-09-11 19:08:19 -0500

    Creación de microservicio Usuarios

---
+++ src/main/java/com/example/Microserviciousuarios/MicroserviciousuariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microserviciousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciousuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciousuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Microserviciousuarios/MicroserviciousuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Microserviciousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciousuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
